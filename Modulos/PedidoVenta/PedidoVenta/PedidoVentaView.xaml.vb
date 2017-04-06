cd2ada56a8a4c38c654680b1de18bd6bb363c6b2
Carlos Adrián Martínez <carlosadrian@nuevavision.es>
2017-04-06 17:40:45 +0200

    PedidoVenta alt C lleva a la cantidad e intro vuelve a llevar al producto (Issue#109)

--- Modulos/PedidoVenta/PedidoVenta/PedidoVentaView.xaml.vb
+++ Modulos/PedidoVenta/PedidoVenta/PedidoVentaView.xaml.vb
@@ -30,6 +30,14 @@ Public Class PedidoVentaView
             DataContext.cmdActualizarTotales.Execute(Nothing)
             actualizarTotales = False
         End If
+
+        Dim currentRowIndex = grdLineas.Items.IndexOf(grdLineas.CurrentItem)
+        If (e.Key = 156 OrElse e.Key = 120) AndAlso currentRowIndex > 1 Then ' Alt + C
+            grdLineas.CurrentCell = New DataGridCellInfo(grdLineas.Items(currentRowIndex - 1), grdLineas.Columns(4)) ' Cantidad
+        End If
+
+
+
     End Sub
 
     Private Sub txtFiltro_GotFocus(sender As Object, e As RoutedEventArgs) Handles txtFiltro.GotFocus
@@ -45,6 +53,14 @@ Public Class PedidoVentaView
             Dim Binding = txtFiltro.GetBindingExpression(TextBox.TextProperty)
             Binding.UpdateSource()
             txtFiltro.SelectAll()
+        End If
+    End Sub
+
+    Private Sub grdLineas_PreviewKeyDown(sender As Object, e As KeyEventArgs) Handles grdLineas.PreviewKeyDown
+        If e.Key = Key.Enter Then
+            If grdLineas.CurrentColumn.Header = "Cantidad" Then
+                grdLineas.CurrentColumn = grdLineas.Columns(2) ' Producto
+            End If
         End If
     End Sub
 
